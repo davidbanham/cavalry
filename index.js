0633d17f4125717a2cbc5ad8410d6ec272c8c5c0
David Banham <david@banham.id.au>
2015-08-28 21:22:04 +1000

    Correct index script

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 var coffee = require('coffee-script');
 if (typeof coffee.register !== 'undefined') coffee.register();
-require(process.argv[2] || './index.coffee');
+require('./index.coffee');
